08c3afd343f46539dba0d4615e4a8c08c9dc9f70
edeustace <ed.eustace@gmail.com>
2018-04-24 13:38:06 -0400

    fix(catalog-demo): fix var name in handleFileSelect

--- src/catalog-demo/index.js
+++ src/catalog-demo/index.js
@@ -1,13 +1,16 @@
-import { DeleteImageEvent, InsertImageEvent } from '@pie-libs/pie-configure-events';
-import { applyStyle, boxShadow, prepareTemplate } from '../styles';
+import {
+  DeleteImageEvent,
+  InsertImageEvent
+} from "@pie-libs/pie-configure-events";
+import { applyStyle, boxShadow, prepareTemplate } from "../styles";
 
-import { ConfigurationPaneUpdateEvent } from './configuration-panes';
-import ElementModels from './element-models';
-import debug from 'debug';
-import merge from 'lodash/merge';
-import range from 'lodash/range';
+import { ConfigurationPaneUpdateEvent } from "./configuration-panes";
+import ElementModels from "./element-models";
+import debug from "debug";
+import merge from "lodash/merge";
+import range from "lodash/range";
 
-const log = debug('pie-catalog-client:catalog-demo');
+const log = debug("pie-catalog-client:catalog-demo");
 
 const templateHTML = `
     <style>
@@ -43,16 +46,16 @@ export default class CatalogDemo extends HTMLElement {
   constructor() {
     super();
 
-    const template = prepareTemplate(templateHTML, 'catalog-demo');
+    const template = prepareTemplate(templateHTML, "catalog-demo");
 
     let sr = applyStyle(this, template);
 
-    this._$itemPreview = sr.querySelector('item-preview');
+    this._$itemPreview = sr.querySelector("item-preview");
 
     this._registeredPies = {};
     this._env = {
-      mode: 'gather'
-    }
+      mode: "gather"
+    };
 
     this.handleFileSelect = this.handleFileSelect.bind(this);
 
@@ -60,17 +63,17 @@ export default class CatalogDemo extends HTMLElement {
   }
 
   connectedCallback() {
-    this._$fileInput.addEventListener('change', this.handleFileSelect);
+    this._$fileInput.addEventListener("change", this.handleFileSelect);
   }
 
   disconnectedCallback() {
-    this._$fileInput.removeEventListener('change', this.handleFileSelect);
+    this._$fileInput.removeEventListener("change", this.handleFileSelect);
   }
 
-  handleFileSelect(e) {
+  handleFileSelect(event) {
     const file = event.target.files[0];
     this._insertImageHandler.fileChosen(file);
-    this._$fileInput.value = '';
+    this._$fileInput.value = "";
     var reader = new FileReader();
     reader.onload = () => {
       const dataURL = reader.result;
@@ -90,14 +93,14 @@ export default class CatalogDemo extends HTMLElement {
       }, n * 20);
     });
 
-    log('[handleFileSelect] reader.readAsDataUrl, file:', file);
+    log("[handleFileSelect] reader.readAsDataUrl, file:", file);
     reader.readAsDataURL(file);
   }
 
   /**
    * Set the controllers map.
-   * 
-   * eg: 
+   *
+   * eg:
    * ```
    * { 'my-pie': { model, outcome } }
    * ```
@@ -108,15 +111,15 @@ export default class CatalogDemo extends HTMLElement {
 
   /**
    * Set the pie item markup.
-   * eg: 
+   * eg:
    * ```html
    *   <catalog-demo>
    *     <div slot="preview">
-   *       <my-pie pie-id="1"></my-pie> 
+   *       <my-pie pie-id="1"></my-pie>
    *     </div>
    *   </catalog-demo>
    * ```
-   * 
+   *
    * > Note: the markup must remain in the light dom to allow styling to take effect
    */
   set markup(m) {
@@ -130,20 +133,20 @@ export default class CatalogDemo extends HTMLElement {
       this.removeChild(existing);
     }
 
-    const div = document.createElement('div');
-    div.setAttribute('slot', 'preview');
+    const div = document.createElement("div");
+    div.setAttribute("slot", "preview");
     div.innerHTML = this._markup;
     this.appendChild(div);
   }
 
   /**
    * Set a map that defines a mapping between a pie's custom element to its configuration element.
-   * 
-   * eg: 
+   *
+   * eg:
    * ```
    * { 'my-pie' : 'my-pie-configuration' }
    * ```
-   * 
+   *
    * If this is not set the demo will only render the pies and the control panel.
    * If this is set it'll render the configuration elements as well.
    * @param m the configure map.
@@ -154,7 +157,7 @@ export default class CatalogDemo extends HTMLElement {
   }
 
   /**
-   * Set the pie item config: 
+   * Set the pie item config:
    * ```javascript
    * { elements, models: [{id, element, ...}, ...] }
    * ```
@@ -170,9 +173,8 @@ export default class CatalogDemo extends HTMLElement {
     }
   }
 
-
   /**
-   * Set the pie item session: 
+   * Set the pie item session:
    * ```javascript
    * [   { id:string, ...}, ...  ]
    * ```
@@ -183,65 +185,65 @@ export default class CatalogDemo extends HTMLElement {
   }
 
   _addConfigurationPanes() {
-
     if (!this._configureMap || !this._config || !this._config.models) {
       return;
     }
 
     let panes = this._config.models.map(cfg => {
-      let configElement = this._configureMap[cfg.element] || 'json-configuration';
+      let configElement =
+        this._configureMap[cfg.element] || "json-configuration";
 
       return `<configuration-pane 
         element-id="${cfg.id}"
         element-name="${cfg.element}">
         <${configElement}></${configElement}>
-      </configuration-pane>`
+      </configuration-pane>`;
     });
 
     //create demo pane for config panes and add to the shadow dom...
-    let demoPane = document.createElement('demo-pane');
-    demoPane.setAttribute('title', 'configuration');
-    let slot = document.createElement('slot');
-    slot.setAttribute('name', 'configure');
+    let demoPane = document.createElement("demo-pane");
+    demoPane.setAttribute("title", "configuration");
+    let slot = document.createElement("slot");
+    slot.setAttribute("name", "configure");
     demoPane.appendChild(slot);
     this.shadowRoot.insertBefore(demoPane, this.shadowRoot.firstChild);
 
     //add the configure panes to the light dom inside a div pointing to the configure slot...
-    let div = document.createElement('div');
-    div.setAttribute('slot', 'configure');
-    div.innerHTML = panes.join('\n');
+    let div = document.createElement("div");
+    div.setAttribute("slot", "configure");
+    div.innerHTML = panes.join("\n");
     this.appendChild(div);
 
-    customElements.whenDefined('configuration-pane')
-      .then(() => {
-        let paneList = this.querySelectorAll('configuration-pane');
+    customElements.whenDefined("configuration-pane").then(() => {
+      let paneList = this.querySelectorAll("configuration-pane");
 
-        for (var i = 0; i < paneList.length; i++) {
-          let p = paneList[i];
-          let m = this._config.models.find(m => m.id === p.getAttribute('element-id'));
-          p.model = m;
-        }
-
-      });
+      for (var i = 0; i < paneList.length; i++) {
+        let p = paneList[i];
+        let m = this._config.models.find(
+          m => m.id === p.getAttribute("element-id")
+        );
+        p.model = m;
+      }
+    });
 
     //Add insert.image event handler, that'll just use a data ure
 
     this.addEventListener(InsertImageEvent.TYPE, e => {
-      log('insert.image event received..');
+      log("insert.image event received..");
       this._insertImageHandler = e.detail;
       this._$fileInput.click();
     });
 
     this.addEventListener(DeleteImageEvent.TYPE, e => {
       const { done, src } = e.detail;
-      log('delete.image event received..', src);
-      log('simulate delete async task');
+      log("delete.image event received..", src);
+      log("simulate delete async task");
       setTimeout(() => {
         done(null);
-      }, 2000)
+      }, 2000);
     });
 
-    this.addEventListener(ConfigurationPaneUpdateEvent.TYPE, (e) => {
+    this.addEventListener(ConfigurationPaneUpdateEvent.TYPE, e => {
       let { id, element, update, reset } = e.detail;
       let index = this._config.models.findIndex(m => m.id === id);
       update = merge(update, { id, element });
